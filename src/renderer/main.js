3e6eff98d21562093372065b23fb4d5be2f1c068
Craig Edwards <craigroe7@gmail.com>
2018-05-13 13:11:26 -0400

    new socket address

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,22 +1,21 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import VueSocketio from 'vue-socket.io';
+import Vue from "vue";
+import Vuetify from "vuetify";
+import VueSocketio from "vue-socket.io";
 
-import App from './App';
-import router from './router';
-import {store} from './../store/store';
+import App from "./App";
+import router from "./router";
+import { store } from "./../store/store";
 
-
-import('../../node_modules/vuetify/dist/vuetify.min.css');
+import("../../node_modules/vuetify/dist/vuetify.min.css");
 
 Vue.use(Vuetify);
 
 // connect to socket
 // Vue.use(VueSocketio, 'https://desolate-lake-73523.herokuapp.com/');
-Vue.use(VueSocketio, 'ec2-54-224-58-35.compute-1.amazonaws.com:3000');
+Vue.use(VueSocketio, "ec2-34-228-29-156.compute-1.amazonaws.com:3000");
 // Vue.use(VueSocketio, 'http://localhost:3000');
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
+if (!process.env.IS_WEB) Vue.use(require("vue-electron"));
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
@@ -24,5 +23,5 @@ new Vue({
   components: { App },
   router,
   store,
-  template: '<App/>',
-}).$mount('#app');
+  template: "<App/>"
+}).$mount("#app");
